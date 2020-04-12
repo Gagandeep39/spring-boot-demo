6051ebf2a96901d845a2240db01e1e380ca82390
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-04-12 23:17:26 +0530

    Created security confi for role based auth Login form is now shown when actuator is accessed

---
+++ src/main/java/com/spring/gagan/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+/**
+ * Gagandeep
+ * 10:30:38 pm
+ * 12-Apr-2020
+ */
+package com.spring.gagan.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * TO configure security
+ *
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	/**
+	 * TO enable role based security
+	 * NOTE: Enable formLogin() when role based authentication is on
+	 * Login page is dilayed when role based pages are accessed and no user is logged in
+	 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/actuator/**").hasRole("ADMIN")
+		.and()
+		.formLogin().permitAll();
+	}
+
+}
